ec67df907b03ab4d6d75f8d3ac7f882a4a2f4a7e
DamianMaj <majutg@wp.pl>
2018-12-03 20:52:34 +0100

    zad.7.3.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,18 +3,19 @@ package com.kodilla.stream;
 import java.util.List;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        ForumUser theForumUser = new ForumUser();
-        List<Forum>theResultOfForum = theForumUser.getList().stream()
-                .filter(forum -> forum.getSex.equals('M'))
-                .filter(forum-> forum.numberOfPost() > 1)
+        Forum theForumUser = new Forum();
+        Map<Integer, ForumUser> theResultOfForum = theForumUser.getList().stream()
+                .filter(forum -> forum.getSex()=='M')
+                .filter(forum-> forum.getNumberOfPost() > 1)
                 .filter(forum -> forum.getBirthDate() > 1998)
-                .map(Forum::toString)
-                .collect(Collectors.joining(", \n", "<<",">>"));
+                .collect(Collectors.toMap(ForumUser::getId, user-> user));
 
         System.out.println(theResultOfForum);
 
